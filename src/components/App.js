a501739bc302461bb1cb513b05fd2861f36910a3
colinkanobe <30920587+colinkanobe@users.noreply.github.com>
2019-10-08 13:04:41 -0400

    commit changes for UI pages and using routing on pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,27 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
-
 import CoursesPage from "./CoursesPage";
+import PageNotFound from "./PageNotFound";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  function getPage() {
+  /*function getPage() {
     const route = window.location.pathname;
     if (route === "/courses") return <CoursesPage />;
     if (route === "/about") return <AboutPage />;
     return <HomePage />;
   }
-
+*/
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PageNotFound extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Page not Found</h2>
+        <p>This app uses React</p>
+      </>
+    );
+  }
+}
+export default PageNotFound;
